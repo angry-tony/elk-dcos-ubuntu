af0d3e8bc08dc98be335d804d3f0ac270fe9b534
root <hee4004@gmail.com>
2017-07-28 15:03:32 +0900

    hoho

--- deploy/filebeat/copyPrerequisitesNodes.sh
+++ deploy/filebeat/copyPrerequisitesNodes.sh
@@ -4,7 +4,7 @@ rm ../yourKey/PLACEyourKEYhere
 ssh-add ../yourKey/!PRIVATEKEYNAME!
 # Will be prompted for you passphrase
 
-sudo apt-get install jq -y
+sudo yum install jq -y
 curl --unix-socket /run/dcos/3dt.sock http://localhost/system/health/v1/nodes | jq '.nodes[].host_ip' | sed 's/\"//g' | sed '/172/d' > nodes.txt
 
 cat nodes.txt | while read line
--- deploy/infrastructure/deploy.sh
+++ deploy/infrastructure/deploy.sh
@@ -15,6 +15,8 @@ sed -i 's/!ELASTICEXECUTORNAME!/'${ELASTIC_EXECUTOR_NAME}'/g' kibanadeploy.json
 sed -i 's/!KIBANANAME!/'${KIBANA_NAME}'/g' kibanadeploy.json
 sed -i 's/!KIBANAFQDN!/'${KIBANA_FQDN}'/g' kibanadeploy.json
 
+sed -i 's/!ELASTICNAME!/'${ELASTIC_NAME}'/g' ../filebeat/deployFilebeatNodes.sh
+sed -i 's/!ELASTICEXECUTORNAME!/'${ELASTIC_EXECUTOR_NAME}'/g' ../filebeat/deployFilebeatNodes.sh
 sed -i 's/!PRIVATEKEYNAME!/'${PRIVATE_KEY_NAME}'/g' ../filebeat/copyPrerequisitesNodes.sh
 
 curl -X POST --data-binary @elasticdeploy.json http://marathon.mesos:8080/v2/apps --header "Content-Type:application/json"
