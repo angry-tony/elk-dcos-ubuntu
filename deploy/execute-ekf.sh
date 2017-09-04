c352295e78b7767e9e54b515862885a1e373ee21
HoHo Kim <hee4004@gmail.com>
2017-09-04 16:09:06 +0900

    Update execute-ekf.sh

--- deploy/execute-ekf.sh
+++ deploy/execute-ekf.sh
@@ -8,15 +8,18 @@ ELA_NETWORK_HOST=192.0.220.104  # slave-node-ip
 
 KIBANA_SERVICE_NAME=kibana
 
+cp elasticdeploy.json elasticdeploy.json.BAK
 sed -i 's/CHANGE_ELASTIC_SERVICE_NAME/'${ELASTIC_SERVICE_NAME}'/g' elasticdeploy.json
 sed -i 's/CHANGE_HOST_VOLUMES_PATH/'${HOST_VOLUMES_PATH}'/g' elasticdeploy.json
 sed -i 's/CHANGE_CONTAINER_VOLUMES_PATH/'${CONTAINER_VOLUMES_PATH}'/g' elasticdeploy.json
 sed -i 's/CHANGE_ELA_CLUSTER_NAME/'${ELA_CLUSTER_NAME}'/g' elasticdeploy.json
 sed -i 's/CHANGE_ELA_NETWORK_HOST/'${ELA_NETWORK_HOST}'/g' elasticdeploy.json
 
+cp kibanadeploy.json kibanadeploy.json.BAK
 sed -i 's/CHANGE_ELASTIC_SERVICE_NAME/'${ELASTIC_SERVICE_NAME}'/g' kibanadeploy.json
 sed -i 's/CHANGE_KIBANA_SERVICE_NAME/'${KIBANA_SERVICE_NAME}'/g' kibanadeploy.json
 
+cp filebeatdeploy.sh filebeatdeploy.sh.BAK
 sed -i 's/CHANGE_ELASTIC_SERVICE_NAME/'${ELASTIC_SERVICE_NAME}'/g' filebeatdeploy.sh
 
 #curl -X POST --cacert /tmp/dcos-ca.crt -H "Authorization: token=$(dcos config show core.dcos_acs_token)" --data-binary @elasticdeploy.json https://marathon.mesos:8443/v2/apps -H 'Content-Type: application/json'
