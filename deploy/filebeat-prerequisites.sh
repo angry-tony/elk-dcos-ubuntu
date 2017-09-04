653968cf5b6c4cd39c0e6a23025feba4cd60fecd
HoHo Kim <hee4004@gmail.com>
2017-09-04 20:21:26 +0900

    Update filebeat-prerequisites.sh

--- deploy/filebeat-prerequisites.sh
+++ deploy/filebeat-prerequisites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install jq -y
+#sudo yum install jq -y
 #curl --unix-socket /run/dcos/3dt.sock http://localhost/system/health/v1/nodes | jq '.nodes[].host_ip' | sed 's/\"//g' | sed '/172/d' > nodes.txt
 
 sudo tee nodes.txt <<-EOF 
